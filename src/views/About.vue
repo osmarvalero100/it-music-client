7b7182f926cc9325b9ce8d0208f4833ac76c05f0
Osmar valero <osmar.valero@farmalisto.com.co>
2019-10-26 20:15:14 -0500

    Creación de menu

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="mt-5">
+    <b-container>
+      <h1>This is an about page</h1>
+    </b-container>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template id="">
+  <div class="mt-5">
+    <b-container>
+      <h1>HOME</h1>
+    </b-container>
+  </div>
+</template>
